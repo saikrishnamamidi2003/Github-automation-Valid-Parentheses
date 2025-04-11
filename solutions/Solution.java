56fcf508bafcb1b5384d873922ec01493368f498
sai krishna <saikrishnamamidi2003@gmail.com>
2025-04-11 16:10:19 +0530

    final commit

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -3,24 +3,6 @@ import java.util.Stack;
 
 public class Solution {
     public static boolean isValid(String s) {
-        // Stack to store opening brackets
-        Stack<Character> stack = new Stack<>();
-        // Loop through the characters in the string
-        for (char c : s.toCharArray()) {
-            // Push opening brackets onto the stack
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                // If the stack is empty or doesn't match, return false
-                if (stack.isEmpty() || 
-                   (c == ')' && stack.pop() != '(') || 
-                   (c == '}' && stack.pop() != '{') || 
-                   (c == ']' && stack.pop() != '[')) {
-                    return false;
-                }
-            }
-        }
-        // Return true if the stack is empty
-        return stack.isEmpty();
+        //write your code
     }
 }
